c8a41a5bda698f3d8658336c146c6d101676ed0d
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-05-14 13:34:57 +0200

    Comienzo ejercicio 2

---
+++ Ejercicio_2/IntegranteSeleccionFutbol.java
@@ -0,0 +1,26 @@
+package Ejercicio_2;
+
+public interface IntegranteSeleccionFutbol {
+
+	public static void concentrarse() {
+		
+		System.out.println("Concentrarse");
+		
+	}
+	public static void viajar() {
+		
+		System.out.println("Viaja");
+		
+	}
+	public static void entrenar() {
+		
+		System.out.println("Entrena");
+		
+	}
+	public static void jugarPartido() {
+		
+		System.out.println("Juega un partido");
+		
+	}
+	
+}
