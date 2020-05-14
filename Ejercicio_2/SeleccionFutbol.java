e8130ae372e88378da071bcb5f893f90fab88b18
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-05-14 13:39:49 +0200

    Super clase abstracta creada

---
+++ Ejercicio_2/SeleccionFutbol.java
@@ -0,0 +1,10 @@
+package Ejercicio_2;
+
+public abstract class SeleccionFutbol implements IntegranteSeleccionFutbol{
+
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	
+}
