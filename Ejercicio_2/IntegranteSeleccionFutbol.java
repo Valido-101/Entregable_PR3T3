6e6fcc3c3851e4bfc5c30a22cd2bc9db4ad6b53c
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-05-15 11:32:23 +0200

    Interfaz del ejercicio 2 corregida

--- Ejercicio_2/IntegranteSeleccionFutbol.java
+++ Ejercicio_2/IntegranteSeleccionFutbol.java
@@ -5,31 +5,10 @@ public interface IntegranteSeleccionFutbol {
 
 	//Métodos que pasarán a la superclase SeleccionFutbol
 	
-	static void concentrarse() {
-		
-		System.out.println("Concentrarse");
-		
-	}
-	static void viajar() {
-		
-		System.out.println("Viaja");
-		
-	}
-	static void entrenar() {
-		
-		System.out.println("Entrena");
-		
-	}
-	static void jugarPartido() {
-		
-		System.out.println("Juega un partido");
-		
-	}
-	
-	static void asistirPartido() {
-		
-		System.out.println("Asiste al partido");
-		
-	}
+	void concentrarse();
+	void viajar();
+	void entrenar();
+	void jugarPartido();
+	void asistirPartido();
 	
 }
